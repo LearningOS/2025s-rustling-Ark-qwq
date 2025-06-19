d38c124265f643d7956aa563de0b43fab4faf461
Ark-qwq <wfz0413@outlook.com>
2025-06-19 14:53:28 +0800

    string and structure

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let s1 = String::from(input);
+    let s2 = String::from(" world!");
+    return s1 + &s2;
+
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut origin = String::from(input);
+    return origin.replace("cars", "balloons");
+
 }
 
 #[cfg(test)]
