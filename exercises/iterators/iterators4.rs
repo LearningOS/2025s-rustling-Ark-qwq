873ee069157361da5f9df8243f97e2ff7b7830fa
Ark-qwq <wfz0413@outlook.com>
2025-06-21 16:32:29 +0800

    complete iterators

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -2,9 +2,6 @@
 //
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -15,6 +12,21 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    // if num == 0 || num == 1 {
+    //     return 1;
+    // } else {
+    //     let mut res = 1;
+    //     let mut count = num;
+    //     loop {
+    //         res *= count;
+    //         count -= 1;
+    //         if count == 0 {
+    //             break;
+    //         }
+    //     }
+    //     res
+    // }
+    (1..=num).fold(1, |acc, x| acc * x)
 }
 
 #[cfg(test)]
